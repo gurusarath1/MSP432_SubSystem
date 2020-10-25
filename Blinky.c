83477648034d1a5315424d51cde434305910498d
Guru Sarath <gurusarath1.official@gmail.com>
2020-10-25 02:01:08 -0500

    Create Blinky.c

---
+++ Blinky.c
@@ -0,0 +1,68 @@
+/*****************************************************************************
+*
+* Copyright (C) 2013 - 2017 Texas Instruments Incorporated - http://www.ti.com/
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* * Redistributions of source code must retain the above copyright
+*   notice, this list of conditions and the following disclaimer.
+*
+* * Redistributions in binary form must reproduce the above copyright
+*   notice, this list of conditions and the following disclaimer in the
+*   documentation and/or other materials provided with the
+*   distribution.
+*
+* * Neither the name of Texas Instruments Incorporated nor the names of
+*   its contributors may be used to endorse or promote products derived
+*   from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+* A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+* OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+* THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+******************************************************************************
+*
+* MSP432 empty main.c template
+*
+******************************************************************************/
+
+#include "msp.h"
+
+void delayX(int iter, int iter2);
+
+int main(void)
+{
+
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;
+
+    P1->DIR |= 0x01;
+
+
+    while(1) {
+        P1->OUT |= 0x01;
+        delayX(1000, 100);
+        P1->OUT &= 0xFE;
+        delayX(1000, 100);
+    }
+
+    return 0;
+}
+
+// Make sure to switch off the optimization
+void delayX(int iter, int iter2) {
+    for(int i=0; i<iter; i++) {
+        for(int j=0; j<iter2; j++) {
+            ;
+        }
+    }
+}
